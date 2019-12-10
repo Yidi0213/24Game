f400b099bd34f3f713796cf21d063ab5510817fb
Yidi Wang <yw73@rice.edu>
2019-12-10 17:51:07 -0600

    Updates

---
+++ precache-manifest.b1e7a9f5437df27f2e3815124d8631bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcae59cefa79ba5243623547b5c28a2f",
+    "url": "/24Game/index.html"
+  },
+  {
+    "revision": "07dfe7238127629f9cd9",
+    "url": "/24Game/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f7943337417c80ef644b",
+    "url": "/24Game/static/css/main.d710d4ce.chunk.css"
+  },
+  {
+    "revision": "07dfe7238127629f9cd9",
+    "url": "/24Game/static/js/2.8f03ddb5.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/24Game/static/js/2.8f03ddb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7943337417c80ef644b",
+    "url": "/24Game/static/js/main.3f8ef2db.chunk.js"
+  },
+  {
+    "revision": "e0dd458e97d20bb3e6c3",
+    "url": "/24Game/static/js/runtime-main.658788e3.js"
+  }
+]);
